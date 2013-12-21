26178f54d6a64dacf1a610cf048a9eae8af02f22
newgudi <newgudi@163.com>
2013-12-21 10:11:18 +0800

    添加后盾框架
    添加后盾框架

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php
+   define("APP_NAME","admin");
+   define("DEBUG",TRUE);
+   define("DIR_SAFE",TRUE);	
+   define('APP_PATH','./admin/' );   
+   include './core/hdphp.php';
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+  define('APP_PATH', './index/');
+
+
+?>
